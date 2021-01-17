3467e0bd9865aeb44303be51e097696d7f4f6be9
Joao Henrique Machado Silva <jmachadosilva@oneconcern.com>
2021-01-17 01:05:58 +0100

    Initial commit and partial implementation

---
+++ src/first.rs
@@ -0,0 +1,46 @@
+use std::mem;
+
+pub struct List {
+    head: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
+
+    pub fn peek(&self) -> Option<i32> {
+        let result;
+        match &self.head {
+            Link::Empty => {
+                result = None
+            }
+            Link::More(ref node) => {
+                result = Some(node.elem);
+            }
+        };
+        result
+    }
+    
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+
