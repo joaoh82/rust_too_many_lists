add985e1623a32ccd1585420019fd44d50f7b1f7
Joao Henrique Machado Silva <joaoh82@gmail.com>
2021-01-24 16:06:40 +0100

    implemented iter_mut

--- src/second.rs
+++ src/second.rs
@@ -20,6 +20,14 @@ impl<T> List<T> {
     }
 }
 
+impl<T> Iterator for IntoIter<T> {
+   type Item = T;
+   fn next(&mut self) -> Option<Self::Item> {
+       // access fields of a tuple struct numerically
+       self.0.pop()
+   }
+}
+
 // Iter is generic over *some* lifetime, it doesn't care
 pub struct Iter<'a, T> {
     next: Option<&'a Node<T>>,
@@ -55,12 +63,25 @@ impl<'a, T> Iterator for Iter<'a, T> {
     }
 }
 
-impl<T> Iterator for IntoIter<T> {
-   type Item = T;
-   fn next(&mut self) -> Option<Self::Item> {
-       // access fields of a tuple struct numerically
-       self.0.pop()
-   }
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut { next: self.head.as_deref_mut() }
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+    
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.elem
+        })
+    }
 }
 
 impl<T> List<T> {
@@ -111,6 +132,20 @@ impl<T> Drop for List<T> {
 #[cfg(test)]
 mod test {
     use super::List;
+
+    #[test]
+    fn iter_mut_test() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
+        assert_eq!(iter.next(), None);
+    }
 
     #[test]
     fn iter_test() {
